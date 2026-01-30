56e85bafb291d91a451d71f560c3654420babee1
Matthias Schmitt <matthias.schmitt@deepl.com>
2026-01-30 19:15:30 +0100

    first person cam

--- player.gd
+++ player.gd
@@ -28,7 +28,7 @@ func _physics_process(delta: float) -> void:
 		animation_player.play("brush_down")
 
 	# Get input direction
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("ui_up", "ui_down", "ui_right", "ui_left")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	# Apply movement
