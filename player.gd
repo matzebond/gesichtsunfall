22e2c10ce43d324c76327b6ad146d8a290c1ab99
Matthias Schmitt <matthias.schmitt@deepl.com>
2026-01-30 19:05:17 +0100

    add player movement

--- player.gd
+++ player.gd
@@ -2,12 +2,46 @@ extends CharacterBody3D
 
 @onready var animation_player = $AnimationPlayer
 
+# Movement settings
+var speed = 5.0
+var jump_velocity = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
+func _physics_process(delta: float) -> void:
+	# Add gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump
+	if Input.is_action_just_pressed("ui_select") and is_on_floor():  # Space bar
+		velocity.y = jump_velocity
+
+	# Handle brush animation
+	if Input.is_action_just_pressed("ui_accept"):  # Enter key
+		animation_player.play("brush_down")
+
+	# Get input direction
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	# Apply movement
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):  # Space bar
-		animation_player.play("brush_down")
+	pass
