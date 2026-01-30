258ae89701fc855433b7c3aa7f8ae127a599bec3
Matthias Schmitt <matthias.schmitt@deepl.com>
2026-01-30 22:11:38 +0100

    look at export target

--- global_player_cam.gd
+++ global_player_cam.gd
@@ -1,7 +1,7 @@
 extends Camera3D
 
-@onready var player: Node3D  = $player
 @export var on: bool = false
+@export var target: Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +11,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	look_at(player.position)
+	look_at(target.position)
