8225981a10c9f9772692bb9b5d50257ef822ea82
Matthias Schmitt <matthias.schmitt@deepl.com>
2026-01-30 21:29:18 +0100

    add gesicht terrain

---
+++ gesicht.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Plane.create_trimesh_collision()
+	$Plane.get_child(0).get_child(0).shape.backface_collision = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
