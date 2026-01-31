82ae4820292d9f8e088550fecfcf012168845b52
johannes <johannes@tpx13>
2026-01-31 02:18:08 +0100

    wenn man in den mund geht wird man jetzt verputzt

---
+++ area_3d.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+func _ready() -> void:
+	$TextureRect.visible = false
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is VehicleBody3D:
+		$TextureRect.visible = true
+		$AudioStreamPlayer.play()
--- gesicht.gd
+++ gesicht.gd
@@ -10,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
