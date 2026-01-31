e2afa0cefae0abff7eeca130928e7014be3adc33
AnJ95 <angeloaracri@yahoo.de>
2026-01-31 15:32:34 +0100

    Allow saving in level editor

--- level/editor/level_editor.gd
+++ level/editor/level_editor.gd
@@ -8,6 +8,7 @@ var distance_threshold = 0.5
 @onready var decal_spawn_point = $DecalSpawnPoint
 @onready var ray_cast: RayCast3D = $RayCast3D
 @onready var camera: Camera3D = $Camera3D
+@export var save_path = "res://level/editor/level.tscn"
 
 func _ready() -> void:
 	$gesicht/Plane.create_trimesh_collision()
@@ -25,3 +26,37 @@ func _process(delta: float) -> void:
 		
 		if ray_cast.is_colliding():
 			decal_spawn_point.global_position = ray_cast.get_collision_point()
+			
+func save():
+	var root = Node3D.new()
+	root.name = "Level"
+	root.owner = root
+	for child in $Decals.get_children():
+		if child == decal_spawner.root_node:
+			continue
+		print("Packing " + child.name)
+		var duplicated_child = child.duplicate()
+		root.add_child(duplicated_child)
+	_set_owner_recursive(root, root)
+	print("Total of %d nodes packed" % root.get_child_count())
+	
+	var scene = PackedScene.new()
+	var ok := scene.pack(root)
+	
+	if ok != OK:
+		printerr("Failed to pack decals scene")
+		return
+
+	var err := ResourceSaver.save(scene, save_path)
+	if err != OK:
+		printerr("Save failed: %s" % err)
+		return
+	
+	scene.take_over_path(save_path)
+	print("Successfully saved to %s (if it does not show/update try leaving and entering godot window again)" % save_path)
+
+
+func _set_owner_recursive(n: Node, owner: Node) -> void:
+	n.owner = owner
+	for c in n.get_children():
+		_set_owner_recursive(c, owner)
