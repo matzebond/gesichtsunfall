9e36508cfc314f67c645f63700d1b913434713e7
AnJ95 <angeloaracri@yahoo.de>
2026-02-01 03:27:30 +0100

    Fix off-by-one in color button

--- ui/color_button.gd
+++ ui/color_button.gd
@@ -7,7 +7,7 @@ var _key_id: int = -1
 		return _key_id
 	set(value):
 		_key_id = value
-		$Label.text = str(value)
+		$Label.text = str(value + 1)
 		$Label/ColorRect.color = Global.BRUSH_COLORS[value]
 
 func on_color_key_id_changed(new_key_id: int):
