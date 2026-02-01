d4143451c8fae5d8c79460db595767bec03501fe
Matthias Schmitt <matthias.schmitt@deepl.com>
2026-02-01 15:49:05 +0100

    repair colliders

--- gesicht/gesicht.gd
+++ gesicht/gesicht.gd
@@ -1,5 +1,16 @@
 extends Node3D
 
+@export var mesh_for_collider: Node
+
 func _ready() -> void:
-	$spahn.create_trimesh_collision()
-	$spahn.get_child(0).get_child(0).shape.backface_collision = true
+	if $spahn:
+		$spahn.create_trimesh_collision()
+		$spahn.get_child(0).get_child(0).shape.backface_collision = true
+
+	if $Plane:
+		$Plane.create_trimesh_collision()
+		$Plane.get_child(0).get_child(0).shape.backface_collision = true
+
+	if $Suzanne:
+		$Suzanne.create_trimesh_collision()
+		$Suzanne.get_child(0).get_child(0).shape.backface_collision = true
