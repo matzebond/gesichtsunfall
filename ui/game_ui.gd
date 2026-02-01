eb2f111a38430bd2c54a90dadfcad0e6dcc2aabe
AnJ95 <angeloaracri@yahoo.de>
2026-02-01 01:22:46 +0100

    Only show top ui during play state

--- ui/game_ui.gd
+++ ui/game_ui.gd
@@ -4,9 +4,12 @@ var game_timer: Timer
 @onready var timer_label: Label = $MarginContainer/VBoxContainer/HBoxContainer/Panel/LabelTime
 @onready var fps_label: Label = $MarginContainer/VBoxContainer/HBoxContainer2/LabelFps
 @onready var score_progress: ProgressBar = $MarginContainer/VBoxContainer/HBoxContainer/ProgressBar
+@onready var top_bar: Control = $MarginContainer/VBoxContainer/HBoxContainer
 
 func _on_game_state_manager_playing_started(game_timer: Timer) -> void:
 	self.game_timer = game_timer
+	timer_label.text = ""
+	top_bar.visible = true
 
 func _on_game_state_manager_playing_done() -> void:
 	self.game_timer = null
@@ -17,7 +20,7 @@ func _process(delta: float) -> void:
 	fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
 
 func _on_game_state_manager_preview_started() -> void:
-	timer_label.text = ""
+	top_bar.visible = false
 
 
 func _on_scoring_percent_changed(percent: float) -> void:
