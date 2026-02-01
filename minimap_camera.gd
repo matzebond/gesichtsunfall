685fcc7f47c66a81e77ddef0e2bfacd09e8d3240
AnJ95 <angeloaracri@yahoo.de>
2026-02-01 15:51:06 +0100

    Hide minimap in scoreing screen

--- minimap_camera.gd
+++ minimap_camera.gd
@@ -1,6 +1,17 @@
 extends Camera3D
 
+@onready var minimap = $"../../Minimap"
 func _process(delta: float) -> void:
 	var playerPos = unproject_position($"../../player".global_position)
-	$"../../PlayerMarker".global_position = $"../../Sprite2D".global_position+playerPos*$"../../Sprite2D".scale.x
-	$"../../PlayerMarker/Label".text = str(playerPos)#/#+$"../../Sprite2D".position
+	$"../../Minimap/PlayerMarker".global_position = minimap.global_position+playerPos * minimap.scale.x
+	$"../../Minimap/PlayerMarker/Label".text = str(playerPos)#/#+$"../../Sprite2D".position
+
+
+func _on_game_state_manager_playing_started(game_timer: Timer) -> void:
+	minimap.visible = true
+
+func _on_game_state_manager_playing_done() -> void:
+	minimap.visible = false
+
+func _on_game_state_manager_preview_started() -> void:
+	minimap.visible = false
